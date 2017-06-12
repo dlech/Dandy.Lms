1e2ff61c37cf0f79a44c859d3e2012939efc5042
David Lechner <david@lechnology.com>
2017-06-11 22:19:11 -0500

    Refactor IExpression into new file

--- src/Lms/Bytecodes/EV3/BytecodeFactory.cs
+++ src/Lms/Bytecodes/EV3/BytecodeFactory.cs
@@ -12,33 +12,6 @@ using Dandy.Lms.Bytecodes.EV3.Opcodes;
 
 namespace Dandy.Lms.Bytecodes.EV3
 {
-    /// <summary>
-    /// Common interface for all bytecode expressions.
-    /// </summary>
-    /// <remarks>
-    /// Expressions are things like variables, unary operators, binary operators, and so on.
-    /// </remarks>
-    public interface IExpression : IByteCode
-    {
-    }
-
-    /// <summary>
-    /// Interface that declares the type returned by an <see cref="IExpression"/> when it is evaluated.
-    /// </summary>
-    /// <typeparam name="T">The type of the expression.</typeparam>
-    public interface IExpression<T> : IExpression where T : VMDataType
-    {
-        /// <summary>
-        /// Gets an index accessor expression.
-        /// </summary>
-        /// <param name="index">The index of an element.</param>
-        /// <returns>A new expression.</returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown if this expression is not a string or an array.
-        /// </exception>
-        IExpression<Data8> this[int index] { get; }
-    }
-    
     enum PrimPar : byte
     {
         Short = 0x00,
---
+++ src/Lms/Bytecodes/EV3/IExpression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dandy.Lms.Bytecodes.EV3
+{
+    /// <summary>
+    /// Common interface for all bytecode expressions.
+    /// </summary>
+    /// <remarks>
+    /// Expressions are things like variables, unary operators, binary operators, and so on.
+    /// </remarks>
+    public interface IExpression : IByteCode
+    {
+    }
+
+    /// <summary>
+    /// Interface that declares the type returned by an <see cref="IExpression"/> when it is evaluated.
+    /// </summary>
+    /// <typeparam name="T">The type of the expression.</typeparam>
+    public interface IExpression<T> : IExpression where T : VMDataType
+    {
+        /// <summary>
+        /// Gets an index accessor expression.
+        /// </summary>
+        /// <param name="index">The index of an element.</param>
+        /// <returns>A new expression.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this expression is not a string or an array.
+        /// </exception>
+        IExpression<Data8> this[int index] { get; }
+    }
+}
