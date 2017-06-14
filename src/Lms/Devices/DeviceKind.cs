762c5fb5e352602850d8faea16dc74de81027e4f
David Lechner <david@lechnology.com>
2017-06-13 21:03:03 -0500

    Add WeDo and Boost device kinds

--- src/Lms/Devices/DeviceKind.cs
+++ src/Lms/Devices/DeviceKind.cs
@@ -40,5 +40,20 @@ namespace Dandy.Lms.Devices
         /// The device is a LEGO MINDSTORMS EV3 intelligent brick in firmware update mode.
         /// </summary>
         EV3FirmwareUpdate,
+
+        /// <summary>
+        /// The device is a LEGO WeDo USB Hub.
+        /// </summary>
+        WeDoUsbHub,
+
+        /// <summary>
+        /// The device is a LEGO WeDo 2.0 Smart Hub.
+        /// </summary>
+        WeDo2SmartHub,
+
+        /// <summary>
+        /// The device is a LEGO Boost Move Hub.
+        /// </summary>
+        BoostMoveHub,
     }
 }
